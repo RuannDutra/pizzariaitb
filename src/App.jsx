93846258d34df3df8232f8dc2fd2ceca98b03747
RuannDutra <rdutrasantos12@gmail.com>
2024-08-01 07:43:51 -0300

    2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import {useStates} from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem';
 
 const App = () => {
 
+  const [color] = useStates("#0000ff")
+  document.body.style.backgroundColor = color
+
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Listagem />} />
     </Routes>
   )
 }
---
+++ src/pages/Listagem.jsx
@@ -0,0 +1,13 @@
+import React, {usaState, useEffect} from 'react'
+
+const Listagem = () => {
+
+    return (
+
+        <>
+            <h2>Pizzas ITB</h2>
+        </>
+    )
+}
+
+export default
