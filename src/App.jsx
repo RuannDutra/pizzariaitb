d3f213d118adb23396a9fb49455912e8f6d997f0
RuannDutra <rdutrasantos12@gmail.com>
2024-08-07 11:04:15 -0300

    Termineeei

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import {useStates} from 'react'
+import {useState} from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Listagem from './pages/Listagem';
+import Listagem  from './pages/Listagem'
+
 
 const App = () => {
 
-  const [color] = useStates("#0000ff")
+  const [color, setColor] = useState("#ffff")
   document.body.style.backgroundColor = color
 
   return (
